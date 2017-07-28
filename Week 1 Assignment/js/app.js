5894dfff727f2e78b4f566e73202526236782a9f
Rawynn <rawynn@gmail.com>
2017-07-28 13:23:47 +0200

    Finished assignment (Lunch Checker): Find out if you eat too much by listing dishes you usually eat for lunch: 1. List items separating them with . , ; 2. Check by clicking on the button I am considering cases when you put an empty string - it doesn't count.

---
+++ Week 1 Assignment/js/app.js
@@ -0,0 +1,49 @@
+(function (){
+  'use strict';
+  angular.module('LunchChecker',[])
+
+  .controller('MsgController', MsgController);
+
+  MsgController.$inject = ['$scope'];
+
+  function MsgController ($scope){
+    $scope.message = "";
+    $scope.lunch="";
+    $scope.color="color:black;"
+    $scope.state="";
+
+    $scope.ifTooMuch = function() {
+      if(calculateIfTooMuch($scope.lunch)<1){
+        $scope.state="has-error";
+        $scope.color="color:red;"
+        return $scope.message= "Please enter data first.";
+      }
+      else if(calculateIfTooMuch($scope.lunch)<=3)
+      {
+        $scope.state="has-success";
+        $scope.color="color:green;"
+        return $scope.message= "Enjoy!";
+      }
+      else if(calculateIfTooMuch($scope.lunch)>3){
+        $scope.state="has-success";
+        $scope.color="color:green;"
+        return $scope.message= "Too much!";
+      }
+    };
+
+    function calculateIfTooMuch(string){
+      if(string==="")
+      {
+        return 0;
+      }
+      else{
+        var splitted = string.split(',').join(',').split('.').join(',').split(';').join(',').split(/,\ +\,/).join(',').split(',').filter(Boolean);
+      //  console.log(splitted.length);
+        console.log(splitted);
+        return splitted.length;
+      }
+
+    };
+
+  }
+  })();
